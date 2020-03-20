7cf12558bd8a0535bd0334a1ea939c8af27ced0f
Javacasm <javacasm@gmail.com>
2020-03-21 00:50:39 +0100

    add

---
+++ code/pines_arduino.h
@@ -0,0 +1,26 @@
+// define IO pin
+
+#define RightLineSensor 10
+#define MediumLineSensor 4
+#define LeftLineSensor 2
+
+#define ServoUltrasonido 3
+
+#define echoUltrasonido A4
+#define trigUltrasonido A5
+
+#define MandoInfraRojo 12
+
+#define ENB 6 //indicamos que vamos a utilizar el cable+
+//ENB(cable que ajusta la frecuencia en la que se enciende y apaga los
+//motores derchos, es decir, la velocidad de los mismos) y este estará conectado al 
+//PIN 6
+#define IN3 9//indicamos que vamos a utilizar el cable IN3 en el PIN 9
+#define IN4 11//indicamos que vamos a utilizar el cable IN4 en el PIN 11
+
+#define ENA 5//indicamos que vamos a utilizar el cable
+//ENA(cable que ajusta la frecuencia en la que se enciende y apaga los
+//motores izquierdos, es decir, la velocidad de los mismos) y este estará conectado al 
+//PIN 5
+#define IN1 7//indicamos que vamos a utilizar el cable IN1 en el PIN 7
+#define IN2 8//indicamos que vamos a utilizar el cable IN2 en el PIN 8
