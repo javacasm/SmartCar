4e82f4a2be19ec92188ec1bf27456e1035c94a47
Javacasm <javacasm@gmail.com>
2020-03-21 00:50:52 +0100

    add

---
+++ code/robot.h
@@ -0,0 +1,14 @@
+#include "pines_arduino.h"
+
+#define VELOCIDADA 200
+#define VELOCIDADB 200
+
+
+
+void robotForward(int duracion);
+void robotLeft(int duracion);
+void robotRight(int duracion);
+void robotBackward(int duracion);
+void robotSmoothstop(int duracion);
+void robotBrake(int duracion);
+void robotSquare();
