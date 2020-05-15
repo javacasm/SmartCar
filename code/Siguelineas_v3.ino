a88363918e335a21608ca3a50bea792875fdfbde
Javacasm <javacasm@gmail.com>
2020-05-16 00:45:15 +0200

    update

--- code/Siguelineas_v3.ino
+++ code/Siguelineas_v3.ino
@@ -17,42 +17,74 @@
 #define WHITE LOW
 
 void setup() {
-  M1Setup();
+  M1Setup(); 
   M1Speed(VELOCIDADA);
   M2Setup();
   M2Speed(VELOCIDADB);
+
+ /* testAdelanteAtras();
+  delay(5000);
+  testCuadrado();
+  delay(5000);   */
+
 }//fin setup
 
-//mian loop
+// Test para comprobar que el software se coordina bien con el hardware
+
+// 1 segundo adelante, para y 1 segundo atras
+void testAdelanteAtras(){
+  robotForwardInstant();
+  delay(1000);
+  robotBrakeInstant();
+  robotBackwardInstant();
+  delay(1000);
+  robotBrakeInstant();
+}
+
+// test del cuadrado
+void testCuadrado(){
+  // repetir x 4 
+    // adelante
+    // derecha
+  robotSetSpeed (150);
+  robotSquare();
+}
+
+#define VELOCIDAD_GIRO_RAPIDO 200
+#define VELOCIDAD_GIRO_LENTO 170
+
+
+//main loop
 void loop() {
 
-  while (LTR==WHITE && LTM==BLACK && LTL==WHITE){
-    robotSetSpeed (120);
+
+  while (LTR == WHITE && LTM == BLACK && LTL == WHITE){
+    robotSetSpeed (VELOCIDAD_GIRO_RAPIDO);
     robotForwardInstant ();
   }
-  //brakeInstant ();
 
-  while (LTR==BLACK){
-    if (LTM==BLACK)robotSetSpeed (100);
-    else robotSetSpeed (120); 
+
+  while (LTR == BLACK){
+    if (LTM == BLACK)robotSetSpeed (VELOCIDAD_GIRO_LENTO);
+    else robotSetSpeed (VELOCIDAD_GIRO_RAPIDO); 
    robotLeftInstant();//ARGUMENTO: En motores=tiempo funcionando
   }
 
-  while (LTL==BLACK){
-    if (LTM==BLACK)robotSetSpeed (100);
-    else robotSetSpeed (120);
+  while (LTL == BLACK){
+    if (LTM == BLACK)robotSetSpeed (VELOCIDAD_GIRO_LENTO);
+    else robotSetSpeed (VELOCIDAD_GIRO_RAPIDO);
    robotRightInstant();
   }
 
-  /*while (LTM==WHITE && LTR==WHITE && LTL==WHITE){
-    robotSetSpeed (120);
+/*
+  while (LTM==WHITE && LTR==WHITE && LTL==WHITE){
+    robotSetSpeed (VELOCIDAD_GIRO_RAPIDO);
     robotRightInstant ();
   }
   while (LTR==BLACK && LTL==BLACK){
-    robotSetSpeed(120);
+    robotSetSpeed(VELOCIDAD_GIRO_RAPIDO);
     robotLeftInstant();
-  }*/
+  }
+*/
 
-  /*robotSetSpeed (150);
-  robotSquare();*/
 }
