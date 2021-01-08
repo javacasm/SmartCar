62b2ee31ae214834056b6fddb4faee39698aa869
Javacasm <javacasm@gmail.com>
2021-01-08 20:55:40 +0100

    update

---
+++ code/aparcado_en_paralelo_v4.1_AJUSTADO/ir_control.h
@@ -0,0 +1,26 @@
+//códigos del control remoto IR
+#define FORWARD 16736925  // FORWARD
+#define BACWARD 16754775  // BACK
+#define LEFT 16720605  // LEFT
+#define RIGHT 16761405  // RIGHT
+#define STOP 16712445  // STOP
+#define UNKNOWN_FORWARD 5316027     // FORWARD
+#define UNKNOWN_BACWARD 2747854299  // BACK
+#define UNKNOWN_LEFT 1386468383  // LEFT
+#define UNKNOWN_RIGHT 553536955   // RIGHT
+#define UNKNOWN_STOP 3622325019  // STOP
+#define KEY1 16738455
+#define KEY2 16750695
+#define KEY3 16756815
+#define KEY4 16724175
+#define KEY5 16718055
+#define KEY6 16743045
+#define KEY7 16716015
+#define KEY8 16726215
+#define KEY9 16734885
+#define KEY0 16730805
+#define KEY_STAR 16728765
+#define KEY_HASH 16732845
+
+//funcion del control remoto IR
+void detect_IR();
