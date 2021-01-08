15725fe0df8659bd7403454524a36d86d185ba85
Javacasm <javacasm@gmail.com>
2021-01-08 20:55:50 +0100

    update

---
+++ code/aparcado_en_paralelo_v4.1_AJUSTADO/constants.h
@@ -0,0 +1,50 @@
+//velocidad de los 4 motores
+#define SLOW 100
+#define MEDIUM 150
+#define FAST 200
+
+//periodos de tiempo (ms)
+#define SPIN 547//para que robot rote 90º
+#define BLINK 3//del seven segments, solo 3 milisegundos para que parezca que todos se encienden a la vez
+#define WAIT 500 
+#define DISPLAY1S 111//para que four_digits_once dure 1 segundo
+
+//ángulos en los que se sitúa el servo para medir
+#define SERVO_RIGHT 20
+#define SERVO_CENTRAL 90
+#define SERVO_LEFT 175
+
+// Número de posición en la tabla asociado a los códigos de los simbolos:
+#define COD_BLANK 0
+#define COD_0 1
+#define COD_1 2
+#define COD_2 3
+#define COD_3 4
+#define COD_4 5
+#define COD_5 6
+#define COD_6 7
+#define COD_7 8
+#define COD_8 9
+#define COD_9 10
+#define COD_A 11
+#define COD_B 12
+#define COD_C 13
+#define COD_D 14
+#define COD_E 15
+#define COD_F 16
+#define COD_G 17
+#define COD_H 18
+#define COD_I 19
+#define COD_J 20
+#define COD_L 21
+#define COD_N 22
+#define COD_Ñ 23
+#define COD_O 24
+#define COD_P 25
+#define COD_Q 26
+#define COD_R 27
+#define COD_T 28
+#define COD_U 29
+#define COD_POCKERFACE 30
+#define COD_SMILE 31
+#define COD_EYES 32
